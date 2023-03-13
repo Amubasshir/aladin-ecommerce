a441532b321d8ecfd8d6e0853c1c93d8a9df9beb
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-13 20:57:38 +0600

    mongodb setup added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import 'componemt/styles/globals.css'
+import 'componemt/styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
