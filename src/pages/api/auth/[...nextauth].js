895cb9f875bd40df33a9d1930dc9ac17f69cd419
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-01 18:29:45 +0600

    twitter provider added for auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,9 +1,9 @@
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth';
-import AppleProvider from 'next-auth/providers/apple';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import TwitterProvider from 'next-auth/providers/twitter';
 import clientPromise from './lib/mongodb';
 
 export default NextAuth({
@@ -15,9 +15,9 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
 
-    AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET,
+    TwitterProvider({
+      clientId: process.env.TWITTER_CONSUMER_KEY,
+      clientSecret: process.env.TWITTER_CONSUMER_SECRET,
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
