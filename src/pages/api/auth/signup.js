0f127f8712d0a3524c8e9f12a1af13dfd6f1b56b
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-04 04:11:53 +0600

    jwt added

--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -1,5 +1,8 @@
+import bcrypt from 'bcrypt';
 import nc from 'next-connect';
+import User from '../../../../models/user';
 import db from '../../../../utils/db';
+import { createActivationToken } from '../../../../utils/token';
 import validateEmail from '../../../../utils/validation';
 const handler = nc();
 handler.post(async (req, res) => {
@@ -12,6 +15,23 @@ handler.post(async (req, res) => {
     if (!validateEmail(email)) {
       return res.status(400).json({ message: 'Invalid email' });
     }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'This email already exists' });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: 'Password must be at least 6 characters' });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ name, email, password: cryptedPassword });
+    const addedUser = await newUser.save();
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    });
+    console.log(activation_token);
+    res.send(activation_token);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
