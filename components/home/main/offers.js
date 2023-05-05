8a022cbee602e040aa867d4f2b03de55db029e5a
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-05 16:25:21 +0600

    offers added to hp

--- components/home/main/offers.js
+++ components/home/main/offers.js
@@ -1,4 +1,26 @@
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './styles.module.scss';
+
+import { Pagination } from 'swiper';
+
 export default function Offers() {
-  return <div className={styles.offers}>offers</div>;
+  return (
+    <div className={styles.offers}>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="offers_swiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
