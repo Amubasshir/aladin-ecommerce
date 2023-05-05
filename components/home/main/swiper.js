948222dea50587053ad7d5f4ca3d5a1fd74e0221
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-05 09:10:32 +0600

    swiper for homepage done

---
+++ components/home/main/swiper.js
@@ -0,0 +1,45 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Autoplay, Navigation, Pagination } from 'swiper';
+
+export default function MainSwiper() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mainSwiper"
+      >
+        <SwiperSlide>
+          <img src="https://i.ibb.co/jR2KQdy/h.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.ibb.co/y0dgCPZ/h7.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.ibb.co/S3Y8B26/h4.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.ibb.co/HHR84dr/h3.jpg" alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
